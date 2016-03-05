c84d19c7b4677532940e695c81ede3a8be8ccd04
edborden <borden.edward@gmail.com>
2016-03-05 12:26:07 -0500

    syntax error

--- app/services/geolocation.js
+++ app/services/geolocation.js
@@ -53,7 +53,7 @@ export default Service.extend({
   },
 
   error(error) {
-    console.warn(`LOCATION ERROR(${error.code}): ${error.message});
+    console.warn(`LOCATION ERROR(${error.code}): ${error.message}`);
   },
 
   getObject() {
