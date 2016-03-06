200f198f493e501d7364680f2fa6bb1e54778559
edborden <borden.edward@gmail.com>
2016-03-06 12:00:19 -0500

    index-route -> search-route

--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('search');
   this.authenticatedRoute('active');
   this.authenticatedRoute('inactive');
   this.authenticatedRoute('queue');
