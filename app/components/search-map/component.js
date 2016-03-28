3351d8b07dd089d465da4633e69effb3a5568c20
edborden <borden.edward@gmail.com>
2016-03-27 21:44:27 -0400

    old search route stuff removed

--- app/components/search-map/component.js
+++ app/components/search-map/component.js
@@ -1,8 +1,7 @@
 import SnaptagMap from '../snaptag-map/component';
 import layout from '../search-map/template';
-import NoLocation from '../../mixins/no-location';
 
-export default SnaptagMap.extend(NoLocation, {
+export default SnaptagMap.extend({
 
   layout,
   model: null
